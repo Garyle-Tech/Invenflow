75fee007152329c54685f6189d95b1e72d4f08fc
Prana dhikasara <pranadhika22@gmail.com>
2025-07-02 16:11:20 +0700

    feat: re initialize mobile app

--- mobile-app/app/src/main/java/id/co/invenflow/MainActivity.kt
+++ mobile-app/app/src/main/java/id/co/invenflow/MainActivity.kt
@@ -11,14 +11,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import id.co.invenflow.ui.theme.MyApplicationTheme
+import id.co.invenflow.ui.theme.InvenflowTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            MyApplicationTheme {
+            InvenflowTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
                         name = "Android",
@@ -41,7 +41,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    MyApplicationTheme {
+    InvenflowTheme {
         Greeting("Android")
     }
 }
